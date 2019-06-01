d66ed2accf6bc3413c8f250199e78e8c9933e375
root <root@ip-172-26-3-71.ec2.internal>
2019-06-01 12:38:35 +0000

    changes image

--- pyre-serve.sh
+++ pyre-serve.sh
@@ -1,13 +1,16 @@
-DIRSRC=~/myuser
-DIRDST=/myuser
-PORT=443
+DIRSRC=~/Sami
+DIRDST=/Sami
+PORT=8300
 CONDAPATH=/opt/conda/bin
 DIRKEY=/etc/letsencrypt
-CERT=$DIRKEY/live/www.mywebsite.com/fullchain.pem
-PKEY=$DIRKEY/live/www.mywebsite.com/privkey.pem
+CERT=$DIRKEY/live/www.ssafadi.xyz/fullchain.pem
+PKEY=$DIRKEY/live/www.ssafadi.xyz/privkey.pem
 #PASS=$(python3 -c "from notebook.auth import passwd;print('c.NotebookApp.password=u\"{}\"'.format(passwd('mypassword')))")
-PASS=c.NotebookApp.password="u\'sha1:f7633984f6a3:8de6afbf4a7fe9b3b0bd86a6ee2d7d6d9231e981\'"
+PASS=c.NotebookApp.password="u\'sha1:f6ddf5b5e7b4:b5c5a36f3264a9272e3cbab4da519d607a8f3c81\'"
 CONFIGFILE=/root/.jupyter/jupyter_notebook_config.py
+KAGGLE_USERNAME=mykagglename
+KAGGLE_KEY=mykagglepasswd
 
-sudo docker run --rm -p $PORT:$PORT -v $DIRSRC:$DIRDST -v $DIRKEY:$DIRKEY samisaf/pyre bash -c \
-    "echo $PASS >> $CONFIGFILE && $CONDAPATH/jupyter lab --port=$PORT --ip=0.0.0.0 --notebook-dir=$DIRDST --no-browser --allow-root --certfile=$CERT --keyfile=$PKEY"
+sudo docker run --name samisaf --rm -d -p $PORT:$PORT -v $DIRSRC:$DIRDST -v $DIRKEY:$DIRKEY -e KAGGLE_USERNAME=$KAGGLE_USERNAME -e KAGGLE_KEY=$KAGGLE_KEY \
+	samisaf/pyre bash -c "echo $PASS >> $CONFIGFILE && $CONDAPATH/jupyter lab --port=$PORT --ip=0.0.0.0 --notebook-dir=$DIRDST --no-browser --allow-root --certfile=$CERT --keyfile=$PKEY"
+
