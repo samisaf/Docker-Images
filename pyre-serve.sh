a1364ff8d490255fa3026c8955de828556945b28
Sami <samisaf@gmail.com>
2019-06-07 18:25:45 +0000

    adds git config

--- pyre-serve.sh
+++ pyre-serve.sh
@@ -10,7 +10,11 @@ PASS=c.NotebookApp.password="u\'sha1:f6ddf5b5e7b4:b5c5a36f3264a9272e3cbab4da519d
 CONFIGFILE=/root/.jupyter/jupyter_notebook_config.py
 KAGGLE_USERNAME=mykagglename
 KAGGLE_KEY=mykagglepasswd
+GIT_E=myemail
+GIT_U=myusername
 
-sudo docker run --name awsomepy --rm -d -p $PORT:$PORT -v $DIRSRC:$DIRDST -v $DIRKEY:$DIRKEY -e KAGGLE_USERNAME=$KAGGLE_USERNAME -e KAGGLE_KEY=$KAGGLE_KEY \
-	samisaf/pyre bash -c "echo $PASS >> $CONFIGFILE && $CONDAPATH/jupyter lab --port=$PORT --ip=0.0.0.0 --notebook-dir=$DIRDST --no-browser --allow-root --certfile=$CERT --keyfile=$PKEY"
+sudo docker run --name awsomepy --rm -d -p $PORT:$PORT -v $DIRSRC:$DIRDST -v $DIRKEY:$DIRKEY \
+	-e KAGGLE_USERNAME=$KAGGLE_USERNAME -e KAGGLE_KEY=$KAGGLE_KEY samisaf/pyre bash -c \
+	"git config --global user.email $GIT_E && git config --global user.name $GIT_U && echo $PASS >> $CONFIGFILE && \
+	$CONDAPATH/jupyter lab --port=$PORT --ip=0.0.0.0 --notebook-dir=$DIRDST --no-browser --allow-root --certfile=$CERT --keyfile=$PKEY"
 
